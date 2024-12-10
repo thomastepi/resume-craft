98f807b0ede7722682920e5c7221026959feabd7
thomastepi <tepexminem2013@gmail.com>
2024-12-10 05:54:36 -0500

    update favicon

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleText = screen.getByText(/AI-Powered Resume Builder/i);
+  expect(titleText).toBeInTheDocument();
 });
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { disableReactDevTools} from '@fvilers/disable-react-devtools';
 
 
@@ -17,5 +16,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
