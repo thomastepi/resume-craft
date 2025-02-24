c8a35f06d92959226188a60c88c6fd3eca25dc09
thomastepi <tepexminem2013@gmail.com>
2025-02-24 15:31:47 -0500

    update unauthorized error msg

--- src/constants/httpErrors.js
+++ src/constants/httpErrors.js
@@ -1,7 +1,7 @@
 export const HTTP_ERRORS = {
   400: "Bad Request: The server could not understand the request.",
   401: "Unauthorized: Please log in to continue.",
-  403: "`You're exploring as a Guest! While you can browse freely, creating, editing, and deleting content requires an account. Sign up for free to unlock all features!`",
+  403: "You're exploring as a Guest! While you can browse freely, creating, editing, and deleting content requires an account. Sign up for free to unlock all features!",
   404: "Not Found: The requested resource was not found.",
   409: "Conflict: The request could not be completed due to a conflict.",
   429: "Too Many Requests: You have exceeded the request limit. Try again later.",
